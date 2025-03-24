d7904f546e2aa442ae9b6f5c221251b08aa61237
Ranjan Pradhan <ranjanpradhan0912@gmail.com>
2025-03-24 16:28:41 +0530

    Configuring Django Admin
    Installed Jazminn frm pip which is an app which helps to customize django admin, made initial migrations and created a superuser and in settings.py file made few customizations for admin section page including site title,site logo,etc

--- ecomprj/ecomprj/settings.py
+++ ecomprj/ecomprj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -134,3 +135,18 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "Ecommerce Admin",
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Ecommerce Admin",
+
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "Ecommerce Admin",
+    
+    # Copyright on the footer
+    "copyright": "Ranjan Pradhan(ranjanpradhan0912@gmail.com)",
+
+ 
+}
