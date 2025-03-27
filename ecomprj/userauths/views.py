f990795c3a3ea1f596a38d9d07ac41ea6d5d294f
Ranjan Pradhan <ranjanpradhan0912@gmail.com>
2025-03-28 00:04:05 +0530

    Registered new user from extended usercreationform
    Post data received from form for new user registration was validated before saving, after successful validation and saving same user was authenticated and logged in and then redirected to the homepage,also flashmessage for successful user registration was displayed

--- ecomprj/userauths/views.py
+++ ecomprj/userauths/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from userauths.forms import UserRegisterForm
+from django.contrib.auth import login,authenticate
+from django.contrib import messages
 
-# Create your views here.
 
 def register_view(request):
 
-    if request.method== "POST":
-        print("User registered successfully")
+    if request.method == "POST":
+        form=UserRegisterForm(request.POST)
+        if form.is_valid():
+            new_user=form.save()
+            username=form.cleaned_data.get("username")
+            messages.success(request,f"Hey{username},your account was successfully created")
+            new_user=authenticate(username=form.cleaned_data['email'],password=form.cleaned_data['password1'])
+            login(request,new_user)
+            return redirect("core:index")
+
+        else:
+            print("Invalid form",form.errors)
+
+       
     else:
-        print("User cannot be registered")
- 
+        pass
+    
     form=UserRegisterForm()
+    
   
     return render(request,"userauths/sign-up.html",{"form":form})
