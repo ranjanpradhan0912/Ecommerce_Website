fc6a7b7897983098c62bd282e319703288432470
Ranjan Pradhan <ranjanpradhan0912@gmail.com>
2025-03-29 23:54:38 +0530

    Create Sign-in Page, Sign-in User and Redirect to Homepage
    Created a new function login_view in userauths app to sign-in user and display the sign-in  page,created a sign-in page inside templates and added url for the same userauths.urls

--- ecomprj/userauths/views.py
+++ ecomprj/userauths/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render,redirect
 from userauths.forms import UserRegisterForm
 from django.contrib.auth import login,authenticate
 from django.contrib import messages
+from django.conf import settings
+
+User=settings.AUTH_USER_MODEL
 
 
 def register_view(request):
@@ -12,7 +15,7 @@ def register_view(request):
             new_user=form.save()
             username=form.cleaned_data.get("username")
             messages.success(request,f"Hey{username},your account was successfully created")
-            new_user=authenticate(username=form.cleaned_data['email'],password=form.cleaned_data['password1'])
+            new_user=authenticate(request,username=form.cleaned_data['email'],password=form.cleaned_data['password1'])
             login(request,new_user)
             return redirect("core:index")
 
@@ -21,9 +24,35 @@ def register_view(request):
 
        
     else:
-        pass
+        form=UserRegisterForm()
     
-    form=UserRegisterForm()
-    
-  
     return render(request,"userauths/sign-up.html",{"form":form})
+
+
+def login_view(request):
+
+    if request.user.is_authenticated:
+        return redirect("core:index")
+    
+    if request.method=="POST":
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+
+        try:
+            user=User.objects.get(email=email)
+        except:
+            messages.warning(request,f"User with {email} does not exist")
+        
+        user=authenticate(request,email=email,password=password)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request,"You are logged in")
+            return redirect("core:index")
+        else:
+            messages.warning(request,"User does not exist,create an account")
+
+    context={
+
+    }
+    return render(request,"userauths/sign-in.html",context)
