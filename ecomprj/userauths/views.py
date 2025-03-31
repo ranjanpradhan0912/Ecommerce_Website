8e61f5883d08f1ef23f33d774194a87727eb87e2
Ranjan Pradhan <ranjanpradhan0912@gmail.com>
2025-03-31 14:31:26 +0530

    Logout Functionality Added
    Created a logout function to logout an authenticated user in the page and in base.html containing nav bar using request.user.is_authenticated function made logout button to appear only when the user is signed in

--- ecomprj/userauths/views.py
+++ ecomprj/userauths/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from userauths.forms import UserRegisterForm
-from django.contrib.auth import login,authenticate
+from django.contrib.auth import login,authenticate,logout
 from django.contrib import messages
 from django.conf import settings
 
@@ -31,6 +31,7 @@ def register_view(request):
 
 def login_view(request):
 
+
     if request.user.is_authenticated:
         return redirect("core:index")
     
@@ -56,3 +57,10 @@ def login_view(request):
 
     }
     return render(request,"userauths/sign-in.html",context)
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request,"User logged out")
+
+    return redirect("userauths:sign-in")
