540d72832face732227a8287f9931209d7d5e365
Ranjan Pradhan <ranjanpradhan0912@gmail.com>
2025-04-01 17:15:12 +0530

    Alert Messages
    Displayed django messages warning or success is base.html inside an alert class. Also used settimeout function using javascript to close alert message after certain mentioned seconds

--- ecomprj/userauths/views.py
+++ ecomprj/userauths/views.py
@@ -3,14 +3,17 @@ from userauths.forms import UserRegisterForm
 from django.contrib.auth import login,authenticate,logout
 from django.contrib import messages
 from django.conf import settings
+from userauths.models import User
 
-User=settings.AUTH_USER_MODEL
+# User=settings.AUTH_USER_MODEL
 
 
 def register_view(request):
 
     if request.method == "POST":
+
         form=UserRegisterForm(request.POST)
+
         if form.is_valid():
             new_user=form.save()
             username=form.cleaned_data.get("username")
@@ -31,27 +34,29 @@ def register_view(request):
 
 def login_view(request):
 
-
     if request.user.is_authenticated:
         return redirect("core:index")
     
     if request.method=="POST":
         email=request.POST.get("email")
         password=request.POST.get("password")
+        print(email)
+        print(password)
 
         try:
             user=User.objects.get(email=email)
+            user=authenticate(request,email=email,password=password)
+            if user is not None:
+                login(request,user)
+                messages.success(request,"You are logged in")
+                return redirect("core:index")
+            else:
+                messages.warning(request,"User does not exist,create an account")
+
         except:
             messages.warning(request,f"User with {email} does not exist")
         
-        user=authenticate(request,email=email,password=password)
-
-        if user is not None:
-            login(request,user)
-            messages.success(request,"You are logged in")
-            return redirect("core:index")
-        else:
-            messages.warning(request,"User does not exist,create an account")
+     
 
     context={
 
