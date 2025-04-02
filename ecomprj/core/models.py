64d697e1b31a4fa0b58181ccc6a2339212a3fd22
Ranjan Pradhan <ranjanpradhan0912@gmail.com>
2025-04-03 00:36:37 +0530

    Created a category model
    Created a category model in core app and also installed a external library called djangoSHORTUUIDFields

--- ecomprj/core/models.py
+++ ecomprj/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
+from django.utils.html import mark_safe
+class Category(models.Model):
+    cat_id=ShortUUIDField(unique=True,length=10,max_length=30,prefix="cat",alphabet="abcdefghijklm")
+    title=models.CharField(max_length=100) #Title,Heading
+    image=models.ImageField(upload_to="category")
 
-# Create your models here.
+    class Meta:
+        verbose_name_plural="Categories"
+    
+    def category_image(self):
+        return mark_safe('<img src="%s" width="50" height="50"/>' %(self.image.url))
+    
+    def __str__(self):
+        return self.title
+
+
