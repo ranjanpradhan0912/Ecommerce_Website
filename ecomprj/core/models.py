94cf4bb401e889a6df979714c0ce706eefe19be7
Ranjan Pradhan <ranjanpradhan0912@gmail.com>
2025-04-03 23:29:06 +0530

    Created a vendor class in models inside core app

--- ecomprj/core/models.py
+++ ecomprj/core/models.py
@@ -1,8 +1,13 @@
 from django.db import models
-from shortuuid.django_fields import ShortUUIDField
+from shortuuidfield import ShortUUIDField
 from django.utils.html import mark_safe
+from userauths.models import User
+
+def user_directory_path(instance,filename):
+    return 'user_{0}/{1}'.format(instance.user.id,filename)
+
 class Category(models.Model):
-    cat_id=ShortUUIDField(unique=True,length=10,max_length=30,prefix="cat",alphabet="abcdefghijklm")
+    cid=ShortUUIDField(unique=True,length=10,max_length=30,prefix="cat",alphabet="abcdefghijklm")
     title=models.CharField(max_length=100) #Title,Heading
     image=models.ImageField(upload_to="category")
 
@@ -14,5 +19,20 @@ class Category(models.Model):
     
     def __str__(self):
         return self.title
+
+class Vendor(models.Model):
+    vid=ShortUUIDField(unique=True,length=10,max_length=30,prefix="ven",alphabet="abcdefghijklm")
+    title=models.CharField(max_length=100) #Title,Heading
+    image=models.ImageField(upload_to=user_directory_path)
+    description=models.TextField(null=True,blank=True)
+    address=models.CharField(max_length=100,default="India") 
+    contact=models.CharField(max_length=100,default"1234567890") 
+    chat_resp_time=models.CharField(max_length=100,default="100") 
+    shipping_on_time=models.CharField(max_length=100,default="100") 
+    authentic_rating=models.CharField(max_length=100,default="100") 
+    days_return=models.CharField(max_length=100,default="100") 
+    warranty_period=models.CharField(max_length=100,default="100") 
+    user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    
 
 
